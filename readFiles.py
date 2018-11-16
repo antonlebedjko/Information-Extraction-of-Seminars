ab265e4fde4311f986ff86dd1856ccb25e281770
axl620 <antonio96@inbox.lv>
2018-11-16 15:50:51 +0000

    time tagger works

--- readFiles.py
+++ readFiles.py
@@ -15,30 +15,18 @@ def timeTagger(email_list):
         pattern = re.compile(r'\b([0-9]{1,2}(?::[0-9]{2}\s?(?:AM|PM|am|pm|a\.m|p\.m)|:[0-9]{2}|\s?(?:AM|PM|am|pm|a\.m|p\.m)))\b')
         for line in email_list:
                 if(not "PostedBy" in line):                                      
-                    matches = pattern.finditer((line))
-                    print(len(list(matches))==1)  
-                    condition = len(list(matches))==1
-                    print(len(list(matches)))
-                    
-                    if(len(list(matches))==1):
-                            print("yo")
-                            line = line[0:match.start()] + "<stime>" + line[match.start():match.end()] + "</stime>" + line[match.end():]
-                            updated_email += line +'\n'
-                    if(int(len(list(matches))) is 1):
-                            print("yo")
-                    if condition is None:
-                            print("poshel nahuj")
-                    "matches = pattern.search(line)"
-                    """
-                    if len(list(matches)) == 0:
-                            updated_email += line + '\n'
-                    else:     
-                        print(len(list(matches)))                  
-                        for match in matches:
-                                print("yoyoyoyoyoyoyoyoyoy\n")
-                                line = line[0:match.start()] + "<stime>" + line[match.start():match.end()] + "</stime>" + line[match.end():]
-                                updated_email += line +'\n'
-                       """
+                        matches = pattern.finditer((line))
+                        "print(len(list(matches))==1)"
+                        "koroche zaceni, otkomentj strochku sverhu i poprobuj zapustitj programmu. Esli eto stroka zakomenchina, to v nachjale teksta mozno uvidetj tagi tipa <stime>, no esli otkomentitj, to boljshe ne rabotaet. Kak? ;DDD"
+                        condition = (len(list(matches))==0)
+                        if (condition):
+                                updated_email += line + '\n'
+                        else:
+                                matches = pattern.finditer((line))
+                                for match in matches:
+                                        
+                                        line = line[0:match.start()] + "<stime>" + line[match.start():match.end()] + "</stime>" + line[match.end():]
+                                        updated_email += line +'\n'
                 else :
                        updated_email += line + '\n'
         return updated_email
