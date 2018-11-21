0b8c6797d120b9a32afe271c74dd976fb4d1136e
axl620 <antonio96@inbox.lv>
2018-11-21 21:19:49 +0000

    fixed minor bugs, improved a bit some taggers, now they are more accurate

--- taggers.py
+++ taggers.py
@@ -1,5 +1,5 @@
 import re
-untagged_email_file = open('seminars_untagged/untagged/363.txt', "r")
+untagged_email_file = open('seminars_untagged/untagged/312.txt', "r")
 
 #helper function to convert a file with an email to a string
 def email_to_string(email_file):
@@ -10,14 +10,16 @@ def email_to_string(email_file):
 
 
 #tagging the end of event times. If we want to tag all time appereances in the text, we should run our endTimeTagger before startTimeTagger, they deppend from each other
-def end_time_tagger(email_list):
-    # converting string with a whole email to list
-    email_list = email_list.split('\n')
+def end_time_tagger(email):
+    # converting string with a whole email to a list
+    email_list = email.split('\n')
     # empty string to append a whole email with tags
     updated_email = ''
     # different patterns, need to evaluate to choose the best one in the end
     pattern = re.compile(r'\b([0-9]{1,2}(?::[0-9]{2}\s?(?:AM|PM|am|pm|a\.m|p\.m)|:[0-9]{2}|\s?(?:AM|PM|am|pm|a\.m|p\.m)))\b')
-    pattern = re.compile('[ ]([-]|(?:-|until))[ ][0-9][0-9]?\:[0-9][0-9][ ][am|AM|pm|PM|a.m|A.M|p.m|P.M]{2,3}')
+    pattern = re.compile('[ ]([-]|(?:-|until))[ ][0-9][0-9]?\:[0-9][0-9][ ][am|AM|pm|PM|a.m|A.M|p\.m\.|p.m|P.M]{2,3}')
+    pattern = re.compile('([ ]{0,1})([-]|(?:-|(?<=until)))([ ]{0,1})[0-9][0-9]?\:[0-9][0-9][ ][am|AM|pm|PM|a.m|A.M|p.m|P.M|]{2,3}')
+    #<etime>- 5:00 PM</etime> need to fix this problem with minus
     for line in email_list:
         if (not "PostedBy" in line):
             matches = pattern.finditer((line))
@@ -27,15 +29,11 @@ def end_time_tagger(email_list):
                 updated_email += line + '\n'
             else:
                 matches = pattern.finditer((line))
-                #because we append to the same string, if we have several matches at the same time, we need to track how many characters we have already appended before that. So, we increase start and end everytime when we add the new tags
-                start = 0
-                end = 0
                 for match in matches:
-                    match_start = match.start() + start
-                    match_end = match.end() + end
-                    line = line[0:match_start] + "<etime>" + line[match_start:match_end] + "</etime>" + line[match_end:]
-                    start += 15 #if we have more than one match in out matches array, we need to update the next match starting index, because we appended extra characters to the string
-                    end += 22 #same with the match end index
+                    end_time = match.group()
+                    print(end_time)
+                    if end_time:
+                        line = line.replace(end_time.strip(), "<etime>" + end_time.strip() + "</etime>")
                 updated_email += line + '\n'
         else:
             updated_email += line + '\n'
@@ -83,7 +81,7 @@ def paragraphs_tagger(email):
         pattern = re.compile(r'(?<=\n\n)(?:(?:\s*\b.+\b:(?:.|\s)+?)|(\s{0,4}[A-Za-z0-9](?:.|\n)+?\s*))(?=\n\n)')
         matches = pattern.finditer(email)
         for match in matches:
-            paragraph = match.group(1)
+            paragraph = match.group(1)         
             if paragraph:
                 email = email.replace(paragraph, "<paragraph>" + paragraph + "</paragraph>")
         return email.strip()
@@ -94,15 +92,16 @@ def sentences_tagger(email):
         #pattern to find the paragraphs, because we have sentences inside them
         pattern = re.compile(r'(?<=\<paragraph\>)(?:(?:\s*\b.+\b:(?:.|\s)+?)|(\s{0,4}[A-Za-z0-9](?:.|\n)+?\s*))(?=\<\/paragraph\>)')
         paragraphs_matches = pattern.finditer(email)
+        sentence_pattern = re.compile(r'(([A-Z][^\.!?]*)(?=[\.!?]))')
         for match in paragraphs_matches:
-            paragraph = match.group(1)
-            if paragraph:                
-                #pattern for finding the sentences in each paragraph
-                sentence_pattern = re.compile(r'(([A-Z][^\.!?]*)(?=[\.!?]))')
+            paragraph = match.group(1)           
+            if paragraph:                 
                 sentences_matches = sentence_pattern.finditer(paragraph)
-                for sentence_match in sentences_matches:
-                        sentence = sentence_match.group()                     
-                        email = email.replace(sentence, "<sentence>" + sentence + "</sentence>")
+                for sentence_match in sentences_matches:                       
+                        sentence = sentence_match.group()   
+                        #to actually tag a sentences, not initials which may appear in a text
+                        if(len(sentence)>1):                                         
+                                email = email.replace(sentence, "<sentence>" + sentence + "</sentence>")
         return email.strip()
         
 
@@ -152,10 +151,10 @@ def location_tagger(email):
 
 def main():
         initial_email = email_to_string(untagged_email_file)
-        untagged_email_file.close()
+        untagged_email_file.close()       
         emailAfterParagraphTagger = paragraphs_tagger(initial_email)
         emailAfterSentencesTagger = sentences_tagger(emailAfterParagraphTagger)
-        emailAfterEndTimeTagger = end_time_tagger(emailAfterSentencesTagger)
+        emailAfterEndTimeTagger = end_time_tagger(emailAfterSentencesTagger)        
         emailAfterStartTimeTagger = start_time_tagger(emailAfterEndTimeTagger)
         email_after_speaker_tagger = speaker_tagger(emailAfterStartTimeTagger)
         email_after_location_tagger = location_tagger(email_after_speaker_tagger)
