e40766a87ec4527bfbb4d6a568f51f31970c61d9
axl620 <antonio96@inbox.lv>
2018-11-22 13:37:25 +0000

    created POS tagger

---
+++ POS_tagger.py
@@ -0,0 +1,32 @@
+import nltk
+
+from nltk.corpus import brown
+from nltk.corpus import treebank
+
+from nltk.tag import DefaultTagger
+
+from nltk.tag import UnigramTagger
+
+from nltk.tag import BigramTagger
+
+from nltk.tag import TrigramTagger
+
+brown.tagged_words(tagset = 'universal')
+
+
+train_sents = treebank.tagged_sents()[:3500]
+test_sents = treebank.tagged_sents()[3500:]
+tagger = DefaultTagger('NN')
+
+
+def back_off_tagger(train_sents,tagger_classes, backoff=None):
+    for cls in tagger_classes:
+        backoff = cls(train_sents,backoff=backoff)
+    return backoff
+
+
+tagger = back_off_tagger(train_sents,[UnigramTagger,BigramTagger,TrigramTagger],backoff=DefaultTagger('NN'))
+
+tagged = tagger.tag(["Jeff", "Schneider", "Research","Associate","Robotics", "Institute"])
+print(tagged)
+print (tagger.evaluate(test_sents))
